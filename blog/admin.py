fcc4dc1d8e6ab797bc3b8247184e24f6c4de6725
Nima77Moosavi <nima77moosavi@gmail.com>
2023-10-11 13:30:05 +0330

    First demo of site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    ordering = ['title']
+    date_hierarchy = 'published_date'
+    empty_value_display = '-empty-'
+    list_display = ['title', 'counted_views', 'status', 'published_date', 'created_date']
+
+admin.site.register(Post, PostAdmin)
